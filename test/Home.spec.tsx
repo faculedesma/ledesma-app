2b7225e070523d7eec63514146d3ad8b3588c8b1
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-12-27 17:42:09 +0100

    add home test & jest configs

---
+++ test/Home.spec.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Home from '../src/components/home/Home';
+import { usePokemons } from '../src/components/hooks/usePokemons';
+import * as hooks from '../src/components/hooks/usePokemons';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+const mockedData = {
+  count: 1154,
+  next: 'https://pokeapi.co/api/v2/pokemon?offset=10&limit=10',
+  previous: null,
+  results: [
+    { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+    { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
+    { name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
+    { name: 'charmeleon', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
+    { name: 'charizard', url: 'https://pokeapi.co/api/v2/pokemon/6/' },
+    { name: 'squirtle', url: 'https://pokeapi.co/api/v2/pokemon/7/' },
+    { name: 'wartortle', url: 'https://pokeapi.co/api/v2/pokemon/8/' },
+    { name: 'blastoise', url: 'https://pokeapi.co/api/v2/pokemon/9/' },
+    { name: 'caterpie', url: 'https://pokeapi.co/api/v2/pokemon/10/' }
+  ]
+};
+
+describe('<Home />', () => {
+  it('fetches all pokemons', async () => {
+    jest.spyOn(hooks, 'usePokemons').mockImplementation(() => ({
+      data: mockedData,
+      isError: false,
+      isLoading: false
+    }));
+    await render(<Home />);
+    expect(usePokemons).toHaveBeenCalledTimes(1);
+    expect(usePokemons).toHaveBeenCalledWith(0);
+  });
+
+  it('shows loading spinner', async () => {
+    jest.spyOn(hooks, 'usePokemons').mockImplementation((offset) => ({
+      data: undefined,
+      isError: false,
+      isLoading: true
+    }));
+    const { getByText } = await render(<Home />);
+    expect(getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('shows error', async () => {
+    jest.spyOn(hooks, 'usePokemons').mockImplementation((offset) => ({
+      data: undefined,
+      isError: true,
+      isLoading: false
+    }));
+    const { getByText } = await render(<Home />);
+    expect(getByText('Error!')).toBeInTheDocument();
+  });
+});
