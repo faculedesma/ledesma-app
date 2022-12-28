243aed0f9f5f0eaa3328a624e9d6885239f69518
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-12-28 10:54:23 +0100

    add gh domain to pack.json & update pipe

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/ledesma-app/',
   plugins: [react(), tsconfigPaths()]
 });
